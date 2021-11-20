2330b1e0d14c8832101cf1f4735d676307f75bba
Arad Arbel <aradarbel10@gmail.com>
2021-11-20 19:32:24 +0200

    reimplemented set as sorted array

--- include/hybrid/set.hpp
+++ include/hybrid/set.hpp
@@ -3,93 +3,116 @@
 #include "array.hpp"
 
 namespace hybrid {
-    namespace impl {
-        template <typename ValT, size_t N = 0>
-        class impl_set : protected hybrid::array<ValT, N> {
-        public:
-            constexpr impl_set() = default;
+	namespace impl {
+		template <typename ValT, size_t N = 0>
+		requires std::totally_ordered<ValT>
+		class impl_set : protected hybrid::array<ValT, N> {
+		private:
+			using iterator = hybrid::array<ValT, N>::iterator;
 
-            constexpr impl_set(std::initializer_list<ValT> il) {
-                for (auto i = il.begin(); i != il.end(); i++) {
-                    insert(*i);
-                }
-            }
+			using hybrid::array<ValT, N>::at;
 
-            using hybrid::array<ValT, N>::size;
-            using hybrid::array<ValT, N>::empty;
-            using hybrid::array<ValT, N>::begin;
-            using hybrid::array<ValT, N>::end;
-            using hybrid::array<ValT, N>::cbegin;
-            using hybrid::array<ValT, N>::cend;
-            using hybrid::array<ValT, N>::rbegin;
-            using hybrid::array<ValT, N>::rend;
+			constexpr int binary_search(const ValT& val, int first, int last) const {
+				if (last == first)
+					return first;
 
-            constexpr void insert(const ValT& val) {
-                if (!contains(val)) {
-                    this->push_back(val);
-                };
-            }
+				auto mid = first + static_cast<int>((last - first) / 2);
+				if (val < at(mid)) {
+					return binary_search(val, first, mid);
+				} else if (val > at(mid)) {
+					return binary_search(val, mid + 1, last);
+				} else { // ==
+					return mid;
+				}
+			}
 
-            constexpr bool contains(const ValT& val) const {
-                for (int i = 0; i < size(); i++) {
-                    if (this->at(i) == val)
-                        return true;
-                }
-                return false;
-            }
+			constexpr int binary_search(const ValT& val) {
+				return binary_search(val, 0, size());
+			}
 
-            constexpr bool erase(const ValT& val) {
-                size_t size_before = size();
-                this->std::vector<ValT>::erase(std::remove(begin(), end(), val));
-                return size() != size_before;
-            }
+		public:
+			constexpr impl_set() { }
 
-            constexpr auto find(const ValT& val) { return std::find(begin(), end(), val); }
-        };
+			constexpr impl_set(std::initializer_list<ValT> il) {
+				for (auto i = il.begin(); i != il.end(); i++) {
+					insert(*i);
+				}
+			}
 
-        template <typename T, size_t S1, size_t S2>
-        constexpr bool operator==(const impl_set<T, S1>& lhs, const impl_set<T, S2>& rhs) {
-            if (lhs.size() != rhs.size())
-                return false;
-            return std::is_permutation(lhs.begin(), lhs.end(), rhs.begin(), rhs.end());
-        }
-    } // namespace impl
+			using hybrid::array<ValT, N>::size;
+			using hybrid::array<ValT, N>::empty;
+			using hybrid::array<ValT, N>::begin;
+			using hybrid::array<ValT, N>::end;
+			using hybrid::array<ValT, N>::cbegin;
+			using hybrid::array<ValT, N>::cend;
+			using hybrid::array<ValT, N>::rbegin;
+			using hybrid::array<ValT, N>::rend;
 
-    template <typename T, size_t N = 0>
-    class set : public impl::impl_set<T, N> {
-    public:
-        constexpr set() = default;
+			constexpr bool insert(const ValT& val) {
+				int index = binary_search(val);
+				if (index < size() && val == at(index)) {
+					return false;
+				}
+				hybrid::array<ValT, N>::insert(begin() + index, val);
+				return true;
+			}
 
-        template <size_t S, template <typename, size_t> class C>
-        constexpr set(const C<T, S>& other) {
-            if (other.size() != N)
-                throw std::invalid_argument{"sizes must match!"};
-            std::copy(other.begin(), other.end(), this->begin());
-        }
+			constexpr bool contains(const ValT& val) const {
+				int index = binary_search(val);
+				return at(index) == val;
+			}
 
-        constexpr set(std::initializer_list<T> il) {
-            if (il.size() != N)
-                throw std::invalid_argument{"sizes must match!"};
-            std::copy(il.begin(), il.end(), this->begin());
-        }
-    };
+			constexpr bool erase(const ValT& val) {
+				int index = binary_search(val);
+				if (index < size() && val == at(index)) {
+					std::vector<ValT>::erase(begin() + index);
+					return true;
+				}
+				return false;
+			}
 
-    template <typename T>
-    class set<T, 0> : public impl::impl_set<T> {
-    public:
-        constexpr set() = default;
+			constexpr auto find(const ValT& val) {
+				int index = binary_search(val);
+				return ((index < size() && at(index) == val) ? begin() + index : end());
+			}
+		};
+	} // namespace impl
 
-        constexpr set(std::initializer_list<T> il) : impl::impl_set<T>(il) {}
+	template <typename T, size_t N = 0>
+	class set : public impl::impl_set<T, N> {
+	public:
+		constexpr set() = default;
 
-        template <size_t N>
-        constexpr set(const set<T, N>& other) {
-            this->resize(N);
-            std::copy(other.begin(), other.end(), this->begin());
-        }
-    };
+		template <size_t S, template <typename, size_t> class C>
+		constexpr set(const C<T, S>& other) {
+			if (other.size() != N)
+				throw std::invalid_argument{"sizes must match!"};
+			std::copy(other.begin(), other.end(), this->begin());
+		}
 
-    template <typename T, size_t N>
-    constexpr auto lift(const hybrid::set<T, N>& container) {
-        return container;
-    }
+		constexpr set(std::initializer_list<T> il) {
+			if (il.size() != N)
+				throw std::invalid_argument{"sizes must match!"};
+			std::copy(il.begin(), il.end(), this->begin());
+		}
+	};
+
+	template <typename T>
+	class set<T, 0> : public impl::impl_set<T> {
+	public:
+		constexpr set() = default;
+
+		constexpr set(std::initializer_list<T> il) : impl::impl_set<T>(il) {}
+
+		template <size_t N>
+		constexpr set(const set<T, N>& other) {
+			this->resize(N);
+			std::copy(other.begin(), other.end(), this->begin());
+		}
+	};
+
+	template <typename T, size_t N>
+	constexpr auto lift(const hybrid::set<T, N>& container) {
+		return container;
+	}
 } // namespace hybrid
